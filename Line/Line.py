2fd7535b01166abf74f10853b7ab7f41dc7f587c
凪 千本 <Senbon.Nagi@outlook.com>
2018-04-15 23:44:30 +0800

    添加项目文件。

---
+++ Line/Line.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def LineDetection():
+      img_1 = input('Input image: ')
+      print('OK')
+      img_2 = cv2.imread(img_1, cv2.IMREAD_GRAYSCALE)
+      img = cv2.Canny(img_2, 50, 200)
+      lines = cv2.HoughLinesP(img, rho=1,
+                              theta=np.deg2rad(0.1),
+                              threshold=80, 
+                              minLineLength=250,
+                              maxLineGap=30)
+      
+      fig, ax = plt.subplots(dpi=300)
+      plt.imshow(img_2, cmap='gray')
+      
+      from matplotlib.collections import LineCollection
+      lc = LineCollection(lines.reshape(-1, 2, 2))
+      ax.add_collection(lc)
+      ax.axis('off')
+      plt.show()
+      return 0
+
+if __name__ == '__main__':
+      LineDetection()
